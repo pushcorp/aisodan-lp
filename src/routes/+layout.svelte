596d68028d1d3f5766b549332df7e5eb6d5d901d
AbeShunya <s.abe@push.co.jp>
2025-08-30 21:02:23 +0900

    feat: add Google Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import "../app.css";
 	import "../styles.css";
+	import Analytics from "$lib/components/Analytics.svelte";
 	import Footer from "$lib/components/Footer.svelte";
 	import Header from "$lib/components/Header.svelte";
 	let { children } = $props();
 </script>
+
+<Analytics />
 
 <Header />
 <main>
