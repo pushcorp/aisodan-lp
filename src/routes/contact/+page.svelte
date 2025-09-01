6f6a9d017d3000c25cec5b3918bb12c0b5b29fdf
AbeShunya <s.abe@push.co.jp>
2025-09-01 10:51:56 +0900

    contact form

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<div class="container mx-auto p-1">
+  <iframe
+    title="contactform"
+    src="https://pushcorp.notion.site/ebd/25204db1911c8013b857c6659ac45180"
+    width="100%"
+    height="600"
+    frameborder="0"
+    allowfullscreen
+  />
+</div>
